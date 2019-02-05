c886f09ef71227fd53027719a1aacb89534c60e3
Lucine Shahgholian <luc.golian@gmail.com>
2019-02-05 10:42:55 +0100

    playboard

--- yahtseeButton.js
+++ yahtseeButton.js
@@ -1,23 +1,27 @@
 
  document.addEventListener("DOMContentLoaded", function(event){
-
     let throwDice=document.getElementById("throwDice");
    
     
     function getRandomInt(min,max){
         return Math.floor(Math.random()*(max-min))+min;
     };
+
    throwDice.addEventListener("click", function(event){ 
-       
+    
        
         let textbox2=document.getElementById("textbox2");
         let textbox3=document.getElementById("textbox3");
         let textbox4=document.getElementById("textbox4");
         let textbox5=document.getElementById("textbox5");
+let playboardsum=document.getElementById("darkerSum");
+
+       
 
         if(document.getElementById("checkbox1").checked == false){
             let textbox1=document.getElementById("textbox1");
             textbox1.value=getRandomInt(1,7);
+
 
         }
         if(document.getElementById("checkbox1").checked == false){
@@ -39,13 +43,12 @@
             let textbox5=document.getElementById("textbox5");
             textbox5.value=getRandomInt(1,7);
 
-        }
-
-
-        
+    }
+    let sumTextbox=document.getElementById("sumTextbox");
+    sumTextbox.value=parseInt(textbox1.value)+parseInt(textbox2.value)+parseInt(textbox3.value)+parseInt(textbox4.value)+parseInt(textbox5.value);
         
     });
-   
+
 
     
      });
