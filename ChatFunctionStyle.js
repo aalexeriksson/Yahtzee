0ef941b5eb431e464c7a17c8e3578bfcb13f78ce
Alexander Eriksson <aalex.e@hotmail.com>
2019-02-06 15:50:31 +0100

    Boss changes

--- ChatFunctionStyle.js
+++ ChatFunctionStyle.js
@@ -3,12 +3,13 @@ $(document).ready(function(){
             var text = $("#inmsg").val();
             var name = $("#inName").val();
 
+            var chatMessage = $("#messages").append("<tr><td>" + text + "   //" + name + "</td></tr>");
             var msg = $("<p></p>").text(text).addClass("chatFunction");
             var msgName = $("<p></p>").text("- " + name).addClass("nameFunction");
 
-            console.log(msg);
-            $('#messages').append(msg);
-            $('#messages').append(msgName);
+            console.log(chatMessage);
+            
+           
         })
 
     })
--- yahtseeButton.js
+++ yahtseeButton.js
@@ -11,41 +11,6 @@
      
     
     
-      
-
-       
-        if(document.getElementById("checkbox1").checked == false){
-            let textbox1=document.getElementById("textbox1");
-            textbox1.value=getRandomInt(1,7);
-
-
-        }
-        if(document.getElementById("checkbox1").checked == false){
-            let textbox2=document.getElementById("textbox2");
-            textbox2.value=getRandomInt(1,7);
-
-        }
-        if(document.getElementById("checkbox3").checked == false){
-            let textbox3=document.getElementById("textbox3");
-            textbox3.value=getRandomInt(1,7);
-
-        }
-        if(document.getElementById("checkbox4").checked == false){
-            let textbox4=document.getElementById("textbox4");
-            textbox4.value=getRandomInt(1,7);
-
-        }
-        if(document.getElementById("checkbox5").checked == false){
-            let textbox5=document.getElementById("textbox5");
-            textbox5.value=getRandomInt(1,7);
-
-
-        let playboardsum1=document.getElementById("playboardsum1");
-        let playboardsum2=document.getElementById("playboardsum2");
-        let playboardsum3=document.getElementById("playboardsum3");
-        let playboardsum4=document.getElementById("playboardsum4");
-
-
 
         count++;
         console.log(count);
@@ -55,17 +20,14 @@
         if(document.getElementById("checkbox1").checked == false){
             let textbox1=document.getElementById("textbox1");
             textbox1.value=getRandomInt(1,7);
-
-
         }
         if(document.getElementById("checkbox2").checked == false){
             let textbox2=document.getElementById("textbox2");
             textbox2.value=getRandomInt(1,7);
-
         }
         if(document.getElementById("checkbox3").checked == false){
-            let textbox5=document.getElementById("textbox3");
-            textbox5.value=getRandomInt(1,7);
+            let textbox3=document.getElementById("textbox3");
+            textbox3.value=getRandomInt(1,7);
         }
 
         if(document.getElementById("checkbox4").checked == false){
@@ -88,11 +50,17 @@
         textbox3.value=0;
         textbox4.value=0;
         textbox5.value=0;
+        checkbox1.checked=false;
+        checkbox2.checked=false;
+        checkbox3.checked=false;
+        checkbox4.checked=false;
+        checkbox5.checked=false;
+       
     }
     let sumTextbox=document.getElementById("sumTextbox");
     sumTextbox.value=parseInt(textbox1.value)+parseInt(textbox2.value)+parseInt(textbox3.value)+parseInt(textbox4.value)+parseInt(textbox5.value);
         
-}
+
    })
 
 let playboardTextboxSum=document.getElementById("playboardsum1")
